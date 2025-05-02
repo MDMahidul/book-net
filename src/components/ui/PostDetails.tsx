e7ff19b298a9d287b36235dbf84a43086f3d2141
MDMahidul <mmbmahidul007@gmail.com>
2025-05-03 01:08:27 +0600

    added comment adding method

--- src/components/ui/PostDetails.tsx
+++ src/components/ui/PostDetails.tsx
@@ -1,11 +1,31 @@
-import { getCommentsById } from "@/services/CommentsServices";
-import { TPost } from "@/types";
+import { createComment, getCommentsById } from "@/services/CommentsServices";
+import { TComment, TPost } from "@/types";
 import Image from "next/image";
 import React from "react";
 import Comments from "./Comments";
+import { revalidateTag } from "next/cache";
 
-const PostDetails =async ({ post }: { post: TPost }) => {
-  const comments=await getCommentsById(post.id, true);
+const PostDetails = async ({ post }: { post: TPost }) => {
+  const comments = await getCommentsById(post.id, true);
+
+  const handleSubmitComment = async (formData: FormData) => {
+    "use server";
+    const commentData = {
+      postId: post.id,
+      comment: formData.get("comment"),
+      id: JSON.stringify(comments.length + 1),
+    };
+    console.log(commentData);
+    try {
+      const res = await createComment(commentData as TComment);
+      if(res){
+        revalidateTag("Comments");
+        console.log("Comment created successfully");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="card card-side bg-base-100 shadow-xl">
@@ -19,6 +39,22 @@ const PostDetails =async ({ post }: { post: TPost }) => {
         </div>
         <p>{post.description}</p>
         <Comments comments={comments} />
+        <form action={handleSubmitComment} className="p-5 mt-12">
+          <div className="form-control">
+            <textarea
+              name="comment"
+              placeholder="Write your comment here..."
+              className="textarea textarea-bordered"
+              required
+            />
+          </div>
+
+          <div className="form-control mt-6">
+            <button type="submit" className="btn btn-accent btn-outline">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/services/CommentsServices/index.ts
+++ src/services/CommentsServices/index.ts
@@ -1,10 +1,16 @@
 "use server";
 
+import { TComment } from "@/types";
 import { delay } from "@/utils/delay";
 
 export const getCommentsById = async (ipostId: string, wait = false) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_API}/comments?postId=${ipostId}`
+    `${process.env.NEXT_PUBLIC_BASE_API}/comments?postId=${ipostId}`,
+    {
+      next: {
+        tags: ["Comments"],
+      },
+    }
   );
   if (!res.ok) {
     throw new Error("Failed to fetch post data");
@@ -12,6 +18,27 @@ export const getCommentsById = async (ipostId: string, wait = false) => {
 
   if (wait) {
     delay(2000);
+  }
+
+  return res.json();
+};
+
+export const createComment = async ( data: TComment) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API}/comments`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(`API Error ${res.status}: ${errorText}`);
   }
 
   return res.json();
